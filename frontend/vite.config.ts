5351c5a98884d97030b01d79a60770ca99fd53c5
Max <mkv1997@yandex.ru>
2025-11-26 17:33:11 +0300

    VR-24: Обновил конфиг vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-  },
+export default defineConfig(({ mode }) => {
+  // Папка, где лежит .env
+  const rootEnvDir = path.resolve(__dirname, '..');
+
+  // Подгружаем переменные из этой папки
+  const env = loadEnv(mode, rootEnvDir, '');
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+      strictPort: true,
+      watch: {
+        usePolling: env.VITE_USE_POLLING === 'true',
+      },
+    },
+  };
 });
