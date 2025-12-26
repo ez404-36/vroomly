385f88d88e40400b025157c6bcf6f559ff27f859
Maxim <mkv1997@yandex.ru>
2025-12-26 17:27:31 +0300

    vr-88 и vr-89: настройка переменных в проекте и светлой / темной темы (#19)

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,10 @@
 import AppRoutes from '../AppRoutes/AppRoutes';
-import '../styles/App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <AppRoutes />
     </>
   );
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/index.css';
 import App from './components/App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
-
-const theme = createTheme({
-  fontFamily: 'Open Sans, sans-serif',
-  primaryColor: 'cyan',
-});
+import { theme } from './styles/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
-        <MantineProvider theme={theme} defaultColorScheme="dark">
-          <App />
-        </MantineProvider>
+        <>
+          <ColorSchemeScript defaultColorScheme="dark" />
+          <MantineProvider theme={theme}>
+            <App />
+          </MantineProvider>
+        </>
       </BrowserRouter>
     </StrictMode>
   </Provider>,
